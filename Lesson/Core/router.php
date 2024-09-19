76bfddb45880639437a8627a18f3450961264793
Administrator <admin@example.com>
2024-09-19 16:54:40 +0300

    Namespacing: What, Why, How?

--- Lesson/Core/router.php
+++ Lesson/Core/router.php
@@ -1,10 +1,9 @@
 <?php
 
-$routes = require ('routes.php');
 function routeToController($uri, $routes) {
 
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -16,6 +15,7 @@ function abort($code = 404) {
     die();
 }
 
+$routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
