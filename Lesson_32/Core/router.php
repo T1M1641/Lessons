0ace43c2007f743d561c7d46e2ed3d45e574845f
Administrator <admin@example.com>
2024-09-23 13:47:02 +0300

    Lesson_32

--- Lesson_32/Core/router.php
+++ Lesson_32/Core/router.php
@@ -11,7 +11,7 @@ function routeToController($uri, $routes) {
 
 function abort($code = 404) {
     http_response_code($code);
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
     die();
 }
 
